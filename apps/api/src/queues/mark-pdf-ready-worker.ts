fe23a6c0236cc7b5b841aea42af7a950b315b5dc
Dori Lahav Waisberg <dori.lahav@gmail.com>
2024-07-15 22:14:18 +0300

    wip - need to forward to error queue and test

--- apps/api/src/queues/mark-pdf-ready-worker.ts
+++ apps/api/src/queues/mark-pdf-ready-worker.ts
@@ -2,15 +2,50 @@ import { logger } from '@pdfgen/logging';
 import { PdfGeneratedMessageContent, Worker } from '@pdfgen/queuing';
 import { markPdfDocumentAsReady } from '../repositories/pdf-documents';
 
-const markPdfReady: Worker<PdfGeneratedMessageContent> = async (pdfId, content, ack) => {
+const markPdfReady: Worker<PdfGeneratedMessageContent> = async (pdfId, content, ack, isRetry) => {
   logger.info(`Updating pdf ${pdfId} status to ready...`);
-  
-  const pdfDocument = await markPdfDocumentAsReady(pdfId, content.fileId);
 
-  if (!pdfDocument) {
-    logger.error(`Cannot mark pdf ${pdfId} Ready because it was not found in the DB! This might indicate that there's a bug.`);
+  let pdfDocument;
+  
+  try {
+    pdfDocument = await markPdfDocumentAsReady(pdfId, content.fileId);
+  } catch (error) {
+    logger.error({
+      msg: `Cannot mark Pdf as ready because an error has occurred.`,
+      context: {
+        pdfId,
+        fileId: content.fileId
+      },
+      err: error
+    });
+
+    // TODO: pass to error queue.
 
     return ack.failure();
+  }
+
+  if (!pdfDocument) {
+    if (isRetry) {
+      logger.error({
+        msg: `Cannot mark pdf as ready because it was not found in the DB! This might indicate that there's a bug.`,
+        context: {
+          pdfId,
+          fileId: content.fileId
+        }
+      });
+
+      return ack.failure();
+    }
+
+    logger.warn({
+      msg: `Cannot mark pdf as ready because it was not found in the DB! Retrying...`,
+      context: {
+        pdfId,
+        fileId: content.fileId
+      }
+    });
+
+    return ack.requeue();
   }
 
   logger.info(`Marked pdf ${pdfId} as ready!`);
--- apps/pdf-generator/src/generate-pdf-worker.ts
+++ apps/pdf-generator/src/generate-pdf-worker.ts
@@ -1,37 +1,51 @@
 import { logger } from '@pdfgen/logging';
 import { pdfGeneratedQueue, PdfRequestedMessageContent, Worker } from '@pdfgen/queuing';
-import { generatePdf, ReactPdfContainer } from '@pdfgen/react-pdf';
+import { generatePdf } from '@pdfgen/react-pdf';
 import { fileManager } from './file-manager';
-
-const uploadPdfContent = async (pdfId: string, container: ReactPdfContainer) => {
-  const generatedPdf = await generatePdf(container); // TODO: fix this type
-  const file = await fileManager.upload(`${pdfId}.pdf`, generatedPdf.pdfReadStream);
-
-  return file;
-}
 
 const createPdfWorker: Worker<PdfRequestedMessageContent> = async (pdfId, content, ack) => {
   logger.info(`Generating pdf ${pdfId}...`);
 
   let generatedPdf;
+  let file;
 
   try {
-    generatedPdf = await generatePdf(content.container);
+    generatedPdf = await generatePdf(content.container); // TODO: fix this type
   } catch (error) {
     logger.error({
       msg: `Could not have generated pdf content!`,
       context: {
         pdfId,
         content: content.container
-      }
+      },
+      err: error
     });
+
+    // TODO: publish to error queue.
+    
+    return ack.failure();
+  }
+
+  try {
+    file = await fileManager.upload(`${pdfId}.pdf`, generatedPdf.pdfReadStream);
+  } catch (error) {
+    logger.error({
+      msg: `Could not have uploaded pdf content!`,
+      context: {
+        pdfId,
+        content: content.container
+      },
+      err: error
+    });
+
+    // TODO: publish to error queue.
     
     return ack.failure();
   }
 
   logger.info(`Generated pdf ${pdfId}!`);
   
-  await pdfGeneratedQueue.publish(pdfId, {fileId: persistedFile.id});
+  await pdfGeneratedQueue.publish(pdfId, {fileId: file.id});
 
   return ack.success();
 }
